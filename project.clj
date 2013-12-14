e344dfe473ca35b0446a863d1b2077cea78a41b9
Metin Emiroğlu <amiroff@Metins-MacBook-Pro.local>
2013-12-14 13:33:51 +0200

    Initial basic project creation

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject basic "0.1.0-SNAPSHOT"
+  :description "Basic clojure web app"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]])
